05070bad472ff3b45792ba6a72fb3225990698d2
kuoted <cvsuser@163.com>
2018-05-24 18:02:16 +0800

    完成界面框架，准备添加数据

--- UI/QuotesMain.py
+++ UI/QuotesMain.py
@@ -6,6 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 import os
+import sys
 
 from PyQt5 import QtWidgets, QtGui, uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -19,17 +20,18 @@ import CandlestickItem
 import candle_stick
 
 class QuotesMainWindow( QMainWindow ):
-    def __init__(self, ui_file=None, parent=None):
+    def __init__(self, ui_file='', parent=None):
         super(QuotesMainWindow, self).__init__( parent )
         self.main_window = QtWidgets.QMainWindow( )
-        try:
-            if os.path.exists(ui_file):
-                self.ui = uic.loadUi(ui_file,  self)
-        except:
-            self.setupGUI()
+        
+        if os.path.exists(ui_file):
+            self.ui = uic.loadUi(ui_file,  self)
+        else:
+            self.__setupGUI()
             
-    def setupGUI( self ):
+    def __setupGUI( self ):
         self.ui = UI_MainWindowEx(self)
+        
         cw = QtWidgets.QWidget()
         l = QtWidgets.QVBoxLayout()
         cw.setLayout(l)
@@ -43,38 +45,29 @@ class QuotesMainWindow( QMainWindow ):
         return
 
     def onKLineChanged(self, changed):
-        print(sys._getframe().f_code.co_filename)  #当前位置所在的文件名
-        print(sys._getframe().f_code.co_name)      #当前位置所在的函数名
-        print(sys._getframe().f_lineno)            #当前位置所在的行号
+        print('%s:%d: %s'%(sys._getframe().f_code.co_name, sys._getframe().f_lineno, sys._getframe().f_code.co_name), end='\n', flush=True)
         return
     def onActionRestorePreLayoutTriggered(self, changed):
-        print(sys._getframe().f_code.co_name)      #当前位置所在的函数名
-        print(sys._getframe().f_lineno)            #当前位置所在的行号
+        print('%s:%d: %s'%(sys._getframe().f_code.co_name, sys._getframe().f_lineno, sys._getframe().f_code.co_name), end='\n', flush=True)
         return
     def onActionSaveCurLayoutTriggered(self, changed):
-        print(sys._getframe().f_code.co_name)      #当前位置所在的函数名
-        print(sys._getframe().f_lineno)            #当前位置所在的行号
+        print('%s:%d: %s'%(sys._getframe().f_code.co_name, sys._getframe().f_lineno, sys._getframe().f_code.co_name), end='\n', flush=True)
         return
 
     def onKLinePeriodChanged(min, max):
-        print(sys._getframe().f_code.co_name)      #当前位置所在的函数名
-        print(sys._getframe().f_lineno)            #当前位置所在的行号
+        print('%s:%d: %s'%(sys._getframe().f_code.co_name, sys._getframe().f_lineno, sys._getframe().f_code.co_name), end='\n', flush=True)
         return
     def onActionTriggered( me, action_ ):
-        print('%d: %s'%(sys._getframe().f_lineno, sys._getframe().f_code.co_name), flush=True)
+        print('%s:%d: %s'%(sys._getframe().f_code.co_name, sys._getframe().f_lineno, sys._getframe().f_code.co_name), end='\n', flush=True)
         print( "action_" + action_.objectName())
         return
 
 
 
-
-
 if __name__ == "__main__":
-    import sys
-  
     import qdarkstyle
-    print('%d: %s'%(sys._getframe().f_lineno, sys._getframe().f_code.co_name), flush=True)
-    ui_file = []
+    print('%s:%d: %s\n'%(sys._getframe().f_code.co_filename, sys._getframe().f_lineno, sys._getframe().f_code.co_name))
+    ui_file = ''
     #ui_file = os.path.dirname(__file__) + "/mainwindow.ui"
     app = QApplication(sys.argv)
     ui = QuotesMainWindow( ui_file )
--- UI/UI_MainWindowEx.py
+++ UI/UI_MainWindowEx.py
@@ -1,33 +1,63 @@
-from mainwindow import Ui_MainWindow
+# -*- coding: utf-8 -*-
 
+import qdarkstyle
+from PyQt5 import QtWidgets, QtGui, QtCore, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from mainwindow import Ui_MainWindow
 '''
     create our MainWindow from a sketch created by Qt Designer.
 '''
 class UI_MainWindowEx(Ui_MainWindow):
     def __init__(self, MainWindow):
         super(UI_MainWindowEx, self).__init__( )
-        self.setupUi(MainWindow)
+        self.main_window_ = MainWindow
+        self.__setupGUI( )
+        self.__setupStockListDockWidget()
         
-    def setupUi(self, MainWindow):
-        super(UI_MainWindowEx, self).setupUi(MainWindow)
+    def __setupGUI(self):
+        super(UI_MainWindowEx, self).setupUi(self.main_window_)
+        
+    def __setupStockListDockWidget(self):
+        self.stocks_list_dock_widget_ = QtWidgets.QDockWidget(self.main_window_)
+        '''
+        AllDockWidgetFeatures = 7，DockWidgetClosable = 1，DockWidgetFloatable = 4，DockWidgetMovable = 2
+        DockWidgetVerticalTitleBar = 8
+        NoDockWidgetFeatures = 0
+        '''
+        self.stocks_list_dock_widget_.setFeatures(QtWidgets.QDockWidget.AllDockWidgetFeatures)    
+        self.stocks_list_dock_widget_.setObjectName("stocks_list_dock_1")        
+        self.stocks_list_table_widget_ = QtWidgets.QTabWidget(self.main_window_)
+                
+        self.favorite_stock_table_ = QtWidgets.QTableView()
+        self.shenzhen_stock_table_ = QtWidgets.QTableView()
+        self.shanghai_stock_table_ = QtWidgets.QTableView()
+        self.stocks_list_table_widget_.addTab(self.favorite_stock_table_, '')
+        self.stocks_list_table_widget_.addTab(self.shenzhen_stock_table_, 'shenzhen stock')
+        self.stocks_list_table_widget_.addTab(self.shanghai_stock_table_, 'shanghai stock')
         
         
+        self.stocks_list_dock_widget_.setWidget(self.stocks_list_table_widget_)
+        self.main_window_.addDockWidget(QtCore.Qt.DockWidgetArea(1), self.stocks_list_dock_widget_)
         
+        self.__retranslateUi(self.main_window_)
         
+    def __retranslateUi(self, MainWindow):
+        #super(UI_MainWindowEx, self).retranslateUi(self.main_window_)
+        _translate = QtCore.QCoreApplication.translate
+        self.stocks_list_table_widget_.setTabText(self.stocks_list_table_widget_.indexOf(self.favorite_stock_table_), _translate("MainWindow", "自选股"))
+        self.stocks_list_table_widget_.setTabText(self.stocks_list_table_widget_.indexOf(self.shenzhen_stock_table_), _translate("MainWindow", "深圳A股"))
+        self.stocks_list_table_widget_.setTabText(self.stocks_list_table_widget_.indexOf(self.shanghai_stock_table_), _translate("MainWindow", "上海A股"))
         
-        
-        
+        self.stocks_list_dock_widget_.setWindowTitle(_translate("MainWindow", "股票列表"))
+        return
         
 if __name__ == "__main__":
-    pass
     import sys
     import os
-    import qdarkstyle
-    from PyQt5 import QtWidgets, QtGui, uic
-    from PyQt5.QtWidgets import QApplication, QMainWindow
+    from QuotesMain import QuotesMainWindow
+    
     app = QApplication(sys.argv)
-    win = QMainWindow( )
-    ui = UI_MainWindowEx(win)
-    ui.setupUi(win)
+    win = QuotesMainWindow( )
     win.show()
     sys.exit(app.exec_())
