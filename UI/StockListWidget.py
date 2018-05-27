72627ba4021bd325a80e6ed466c959e1da51cf1c
kuoted <cvsuser@163.com>
2018-05-27 21:43:03 +0800

    add retrieve stock list sketch code.

--- UI/StockListWidget.py
+++ UI/StockListWidget.py
@@ -5,20 +5,24 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import * #QThreadPool, QRunnable, QThread
 
-class __FetchStockList(QRunnable):
-    def __init__(self, parent):
+class FetchStockList(QRunnable):
+    '''
+    @param done: a signal to emit when job done.
+    '''
+    def __init__(self, done=None):
         super(QRunnable, self).__init__()
+        #super(QObject, self).__init__()
+        self.work_obj_ = done
         pass
     def run(self):
         print('%s:%d: %s'%(sys._getframe().f_code.co_filename, sys._getframe().f_lineno, sys._getframe().f_code.co_name))
         print( 'FetchStockList:%s'%(QThread.currentThread().objectName()))
+        # do something
+        self.work_obj_.job_done_.emit()
         return
-        
-    @staticmethod
-    def fetchStockList():
-        pass
 
 class StockListWidget(QObject):
+    job_done_   = pyqtSignal()
     def __init__(self, parent=None):
         super(QObject, self).__init__()
         self.table_widget_ = QTableWidget(2, 12, parent)
@@ -28,6 +32,13 @@ class StockListWidget(QObject):
         #self.table_widget_.setRowCount(2)
         #self.table_widget_.setColumnCount(12)
         self.table_widget_.setEnabled(True)
+        self.job_done_.connect(self.handle_data)
+        QThreadPool.globalInstance().start(FetchStockList( self ), 0)
+    
+    @pyqtSlot()
+    def handle_data():
+        print('%s:%d: %s'%(sys._getframe().f_code.co_filename, sys._getframe().f_lineno, sys._getframe().f_code.co_name))
+        print( 'FetchStockList:%s'%(QThread.currentThread().objectName()))
         
     def widget(self):
         return self.table_widget_
@@ -63,10 +74,7 @@ if __name__ == "__main__":
     
     MainWindow.setCentralWidget( central_widget.widget() )
     MainWindow.show()
-    
-    fs = __FetchStockList( app )
-    qgs = QThreadPool.globalInstance().start(fs, 0)
-    
+       
     ret = app.exec_( )
     QThreadPool.globalInstance().waitForDone(-1)
     sys.exit(ret)
